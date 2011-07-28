82f4e34ac8a5da748474ef2049fefcec45bb9520
Guillaume Boué <guillaumeboue@yahoo.fr>
2011-07-29 00:26:28 +0200

    commentaire

--- django_util/common_utils.py
+++ django_util/common_utils.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 
 def f7(seq):
-    seen = set()
-    seen_add = seen.add
+	"""
+	Make the list unique. called f7 because unique is a reserved word
+	"""
+	seen = set()
+	seen_add = seen.add
 
-    return [ x for x in seq if x not in seen and not seen_add(x)]
-    
+	return [ x for x in seq if x not in seen and not seen_add(x)]
